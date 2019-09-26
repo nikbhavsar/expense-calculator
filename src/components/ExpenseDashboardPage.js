22682868f68f81376ad25d4850ba1483f421738b
Nikhar <bhavsarnikhar21@gmail.com>
2019-09-25 17:14:06 -0700

    Added total Expense component

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpenseSummery from './ExpensesSummery';
 
 const ExpenseDashboardPage = () => (
     <div> 
     <p> This is my expence dashboard page. </p>
+    <ExpenseSummery />
     <ExpenseListFilters />
     <ExpenseList />
     
