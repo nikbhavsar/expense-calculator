7b1873500c41942989d853731db4e9611f2ab916
Nikhar <bhavsarnikhar21@gmail.com>
2019-09-27 14:13:43 -0700

    Added update and remove functionality

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -30,11 +30,31 @@ export const removeExpense = ({ id } = {}) => ({
     id
 });
 
+//START_REMOVE_EXPENSE
+
+export const startRemoveExpense = ({ id } = {}) => {
+    return (dispatch) => {
+      database.ref(`expenses/${id}`).remove().then(()=>{
+          dispatch(removeExpense({id}));
+      });
+    };
+};
+
 export const editExpense = (id, updates) => ({
     type: 'EDIT_EXPENSE',
     id,
     updates
 });
+
+//START_EDIT_EXPENSE
+
+export const startEditExpense = (id,updates) => {
+  return(dispatch) => {
+      return database.ref(`expenses/${id}`).update(updates).then(()=>{
+         dispatch(editExpense(id,updates));
+      });
+  }
+};
 
 //SET_EXPENSES
 export const setExpenses = (expenses) =>({
